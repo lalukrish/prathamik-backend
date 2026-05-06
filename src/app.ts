7cdcc94bf50b44f919a6350f2b82b415448ff0fe
lalkrishnaicg <lalkrishna.tech@deutsche.dk>
2026-05-06 13:47:21 +0530

    no changes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("HAi Backend Running 🚀");
+  res.send("HAi Backend Running 🚀");
 });
 
 export default app;
