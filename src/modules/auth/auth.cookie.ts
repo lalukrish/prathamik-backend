38d970df08c9c1c560d55b602457bc8df7cbf14c
Swafuvancp <swafuvan.tech@deutsche.dk>
2026-05-06 17:15:58 +0530

    fix: access token small changes

--- src/modules/auth/auth.cookie.ts
+++ src/modules/auth/auth.cookie.ts
@@ -9,7 +9,7 @@ export const setAuthCookies = (
         httpOnly: true,
         secure: true,
         sameSite: "strict",
-        maxAge: 24 * 60 * 1000,
+        maxAge: 24 * 60 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
