a42c156cc22be85b0bf8f38ea138b9af82c739cb
Swafuvancp <swafuvan.tech@deutsche.dk>
2026-05-07 09:42:13 +0530

    fix: small changes from cookie

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("HAi Backend Running 🚀");
