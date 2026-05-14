8a9fc382215dd7046756be4ed90d8dddcce32471
lalkrishnaicg <lalkrishna.tech@deutsche.dk>
2026-05-14 14:38:49 +0530

    fix:app fix

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ["http://localhost:3000", "https://your-frontend.vercel.app"],
     credentials: true,
   }),
 );
