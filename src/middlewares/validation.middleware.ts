3da304ac314b1da8eabc94cece4edab926935bef
lalkrishnaicg <lalkrishna.tech@deutsche.dk>
2026-06-10 11:26:13 +0530

    fix:space added

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -1,49 +1,30 @@
-import {
-  NextFunction,
-  Request,
-  Response,
-} from "express";
+import { NextFunction, Request, Response } from "express";
 
-import {
-  ZodError,
-  ZodSchema,
-} from "zod";
+import { ZodError, ZodSchema } from "zod";
 
 export const validate =
   (schema: ZodSchema) =>
-    async (
-      req: Request,
-      res: Response,
-      next: NextFunction,
-    ) => {
-      try {
-        req.body =
-          await schema.parseAsync(
-            req.body,
-          );
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      req.body = await schema.parseAsync(req.body);
 
-        next();
-      } catch (error) {
-        if (error instanceof ZodError) {
-          return res.status(400).json({
-            success: false,
-            message:
-              "Validation failed",
-
-            errors: error.issues.map(
-              (err) => ({
-                field:
-                  err.path.join("."),
-                message: err.message,
-              }),
-            ),
-          });
-        }
-
-        return res.status(500).json({
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return res.status(400).json({
           success: false,
-          message:
-            "Internal server error",
+          message: "Validation failed",
+
+          errors: error.issues.map((err) => ({
+            field: err.path.join("."),
+            message: err.message,
+          })),
         });
       }
-    };
+
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  };
